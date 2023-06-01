63d6a7d4a60d32898ec74e00a5d441ef7bbeff8c
Vedat Eroğlu <vedateroglu@trair.com.tr>
2023-06-01 12:22:04 +0300

    added ejs

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const ejs = require("ejs");
 const path = require("path");
 const app = express();
 
+//template engine
 
+app.set("view engine", "ejs");
+
+// middleware
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  // res.sendFile(path.resolve(__dirname, "temp/index.html"));
+
+  res.render("index");
 });
 
 const port = 3000;
